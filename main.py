54c66b3ce0441f91dbef2c34554996f323a78b81
chrzanof <58990661+chrzanof@users.noreply.github.com>
2024-10-16 09:42:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+splits = {'train': 'train.csv', 'validation': 'dev.csv', 'test': 'test.csv'}
+df = pd.read_csv("hf://datasets/Adapting/empathetic_dialogues_v2/" + splits["train"])
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df.head(10))
