eb196bee71ec5b3d93afb5898745321bf59dafce
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2024-10-23 09:58:27 +0200

    dane polaczone

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pandas as pd
 
-splits = {'train': 'train.csv', 'validation': 'dev.csv', 'test': 'test.csv'}
-df = pd.read_csv("hf://datasets/Adapting/empathetic_dialogues_v2/" + splits["train"])
+# Przygotowanie danych
+splits_1 = {'train': 'train.csv', 'validation': 'dev.csv', 'test': 'test.csv'}
+df_1 = pd.read_csv("hf://datasets/Adapting/empathetic_dialogues_v2/" + splits_1["train"])
 
-print(df.head(10))
+splits_2 = {'train': 'train.jsonl', 'validation': 'validation.jsonl', 'test': 'test.jsonl'}
+df_2 = pd.read_json("hf://datasets/SetFit/emotion/" + splits_2["train"], lines=True)
+
+df_1.drop(columns=['id', 'chat_history', 'sys_response', 'question or not', 'behavior'], inplace=True)
+df_2.drop(columns=['label'], inplace=True)
+
+df_1.rename(columns={'situation': 'text'}, inplace=True)
+df_2.rename(columns={'label_text': 'emotion'}, inplace=True)
+
+print(df_1.columns)
+print(df_2.columns)
+
+combined_df = pd.concat([df_1, df_2])
+
+# Sprawdzenie połączonych
+print(combined_df.head())
