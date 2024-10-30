be63b3f8ac7982c56f5ca46065af60f7972a2ad8
chrzanof <58990661+chrzanof@users.noreply.github.com>
2024-10-30 08:53:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ user_input = st.text_input('Enter a text to recognize emotion')
 st.write(user_input)
 
 if user_input:
-    st.session_state['show_chart'] = st.session_state.get('show_chart', True)
+    st.session_state['show_chart'] = True
+else:
+    st.session_state['show_chart'] = False
+
 
 if st.session_state.get('show_chart'):
     st.bar_chart(chart_data)
