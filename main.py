b6915dcfcdfba6b11a3700df66e940ed795ed8cd
chrzanof <58990661+chrzanof@users.noreply.github.com>
2024-10-30 09:55:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import numpy as np
 import streamlit as st
+from streamlit import columns
 import data_model
 import pandas as pd
 
-# Sprawdzenie połączonych
-st.write(data_model.df_1.columns)
-st.write(data_model.df_2.columns)
-
-st.write(data_model.combined_df.head())
-chart_data = pd.DataFrame({
-    'x': [1, 2, 3, 4],
-    'y': [10, 20, 30, 40]
-})
+#data placeholder
+chart_data = pd.DataFrame(
+    {
+        'emotions': ['Sad', 'Angry', 'Faithful', 'Disgusted'],
+        'probability': [0.1, 0.2, 0.3, 0.4]
+    }
+)
+st.title("Emotion detection app")
 user_input = st.text_input('Enter a text to recognize emotion')
+
+# TODO: Get result from model
+# result = data_model.get_result(user_input)
+# chart_data = pd.DataFrame({<result>})
+
+output = chart_data.style.format({'probability': "{:.1%}".format})
 st.write(user_input)
 
 if user_input:
@@ -20,4 +27,6 @@ else:
     st.session_state['show_chart'] = False
 
 if st.session_state.get('show_chart'):
-    st.bar_chart(chart_data)
+    st.bar_chart(output, x='emotions', y='probability')
+    st.write(output)
+
