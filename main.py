54c84c382a485dbc16bcb0c93f91451ec78dae01
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2025-01-09 21:36:46 +0100

    main update

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import os
 import pandas as pd
 import requests
 import streamlit as st
+import zipfile
 from autogluon.multimodal import MultiModalPredictor
 
-url = 'https://huggingface.co/pncqq/ag_emotion_predictor/resolve/main/ag-20241112_165247/model.ckpt'
-local_path = "AutogluonModels/ag-20241112_165247/model.ckpt"
+zip_url = 'https://huggingface.co/pncqq/emotion_predictor/resolve/main/ag-20241112_165247.zip'
+zip_local  = "ag-20241112_165247.zip"
+target_dir = "AutogluonModels/ag-20241112_165247"
 
-if not os.path.exists(local_path):
-    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+if not os.path.exists(target_dir):
+    # os.makedirs(os.path.dirname(local_path), exist_ok=True)
     print("Downloading model...")
-    r = requests.get(url, stream=True)
-    with open(local_path, "wb") as f:
+    r = requests.get(zip_url, stream=True)
+    with open(zip_local, "wb") as f:
         for chunk in r.iter_content(chunk_size=8192):
             f.write(chunk)
+
+    with zipfile.ZipFile(zip_local, 'r') as zip_ref:
+        zip_ref.extractall("AutogluonModels")
     print("Download complete.")
 
-model = MultiModalPredictor.load(local_path)
+model = MultiModalPredictor.load(target_dir)
 
 st.title("Emotion detection app")
 user_input = st.text_input('Enter a text to recognize emotion')
